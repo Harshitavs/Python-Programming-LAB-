d166759d5ca2c1a7dff004b892c2abc3af50e4f8
Harshita Mathur <mail2harshitamathur@gmail.com>
2026-04-11 23:08:33 +0530

    Add files via upload

---
+++ EXPERIMENT8_Q1.py
@@ -0,0 +1,9 @@
+
+
+from tkinter import *
+root = Tk()
+
+myLabel = Label(root , text="Hello Harshita") #proccess happens in the root (main overview)
+myLabel.pack()  #cant change once packed 
+
+root.mainloop()
